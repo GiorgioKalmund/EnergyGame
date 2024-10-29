21d2e53c00485f0e3c3a63ee164079696d86aef9
Giorgio Kalmund <giorgio@kalmund.com>
2024-10-29 19:20:18 +0100

    added dummy class structure

---
+++ Assets/Scipts/Models/EnergyOccurence.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For example coal mining spot
+public abstract class EnergyOccurence : MonoBehaviour
+{
+    protected EnergyType energyType;
+    protected double amount;
+}
---
+++ Assets/Scipts/Models/EnergyType.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// For example coal
+public abstract class EnergyType : MonoBehaviour
+{
+    protected String title;
+    protected double energyPerUnit;
+}

940e9dfa43c9d02fe2b22256e3b7ecfa2a55d18a
GiorgioKalmund <147076723+GiorgioKalmund@users.noreply.github.com>
2024-12-11 16:34:56 +0100

    feat: continuing on map marker logic

--- Assets/Scipts/Prototyping/Managers/LevelMapManager.cs
+++ Assets/Scipts/Prototyping/Managers/LevelMapManager.cs
@@ -1,19 +1,59 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelMapManager : MonoBehaviour
 {
-    public LevelMapManager Instane { get; private set; }
-    
+    public static  LevelMapManager Instance { get; private set; }
+
+    public List<LevelMapMarker> markers;
+    public int maxMarkerCount;
+
     private void Awake()
     {
-        if (Instane && Instane != this)
+        if (Instance && Instance != this)
         {
             Destroy(this.gameObject);
         }
         else
         {
-            Instane = this;
+            Instance = this;
+        }
+        markers = new List<LevelMapMarker>();
+    }
+
+    private void Start()
+    {
+        // TODO: Implement saving for current progression state
+        while (markers.Count != maxMarkerCount)
+        {
+            
+        }
+        LinkMarkers();
+    }
+
+    public void AddMarker(LevelMapMarker marker)
+    {
+       markers.Add(marker);
+    }
+
+    private void LinkMarkers()
+    {
+        Debug.Log("Linking "+markers.Count + " markers.");
+        markers.Sort((marker0, marker1) => marker0.id.CompareTo(marker1.id));
+        for (int index = 0; index < markers.Count; index++)
+        {
+            if (index != 0)
+            {
+                Debug.Log("Set prev for "+index);
+                markers[index].SetPrev(markers[index-1]);
+            }
+
+            if (index != markers.Count - 1)
+            {
+                Debug.Log("Set next for "+index);
+                markers[index].SetNext(markers[index+1]);
+            }
         }
     }
 }
--- Assets/Scipts/Prototyping/Managers/LevelMapMarker.cs
+++ Assets/Scipts/Prototyping/Managers/LevelMapMarker.cs
@@ -1,6 +1,98 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelMapMarker : MonoBehaviour
 {
-    public bool unlocked;
+    public bool unlocked { get; private set; }
+    public int id;
+
+    [Header("Neighbours")] 
+    [SerializeField] private LevelMapMarker prev;
+    [SerializeField] private LevelMapMarker next; 
+    
+    [Header("Sprites")] 
+    [SerializeField] private Sprite lockedImage;
+    [SerializeField] private Sprite unlockedImage;
+
+    private Image _spriteImage;
+    private Button _button;
+
+    private LineRenderer _connectingLine;
+
+    private void OnEnable()
+    {
+        LevelMapManager.Instance.AddMarker(this);
+        _spriteImage = GetComponent<Image>();
+        _button = GetComponent<Button>();
+        if (!_button)
+        {
+            gameObject.AddComponent<Button>();
+            Debug.LogWarning(name +" didn't have a button component, added one.");
+        }
+        _button.onClick.AddListener(ToggleLock);
+
+        _connectingLine = gameObject.AddComponent<LineRenderer>();
+        _connectingLine.startWidth= 0.1f;
+        _connectingLine.endWidth = 0.1f;
+        _connectingLine.positionCount = 2;
+        _connectingLine.SetPosition(0, gameObject.transform.position);
+        _connectingLine.SetPosition(1, gameObject.transform.position);
+
+        unlocked = false;
+        _spriteImage.sprite = lockedImage;
+    }
+
+    public void Unlock()
+    {
+        if (prev)
+        {
+            prev.Unlock();
+        }
+        unlocked = true;
+        _spriteImage.sprite = unlockedImage;
+    }
+
+    public void Lock()
+    {
+        if (next)
+        {
+            next.Lock();
+        }
+        unlocked = false;
+        _spriteImage.sprite = lockedImage;
+    }
+
+    public void ToggleLock()
+    {
+        if (unlocked)
+        {
+            Lock();
+        }
+        else
+        {
+            Unlock();
+        }
+    }
+
+    public LevelMapMarker GetNext()
+    {
+        return next;
+    }
+    
+    public void SetNext(LevelMapMarker next)
+    {
+        this.next = next;
+        _connectingLine.SetPosition(1, next.gameObject.transform.position);
+    }
+
+    public LevelMapMarker GetPrev()
+    {
+        return prev;
+    }
+    
+    public void SetPrev(LevelMapMarker prev)
+    {
+        this.prev = prev;
+    }
 }

2d4e7633678eeb5238945fd09d39b25cacd2aa87
safetymax <maximilian.grummt@gmx.de>
2024-12-15 14:50:33 +0100

    forgor to save GraphManager (made variable public)

--- Assets/Scipts/Prototyping/Backend_refactor/GraphManager.cs
+++ Assets/Scipts/Prototyping/Backend_refactor/GraphManager.cs
@@ -6,7 +6,7 @@ public class GraphManager : MonoBehaviour
 {
     public int[,] Matrix { get; } = new int[100, 100];
     private Wandler[] wandlerArray = new Wandler[100];
-    private int numOfWandler = 0;
+    public int numOfWandler = 0;
     
     public void InsertNew(Wandler wandler){
         wandlerArray[numOfWandler++] = wandler;

528e1bc4f826ee9db4fa983cbebd4da375ae62b4
Aloyminium <mwendel888@gmail.com>
2025-01-21 15:19:14 +0100

    stromibus levelus numero uno

--- Assets/StructurePrebuilder.cs
+++ Assets/StructurePrebuilder.cs
@@ -9,14 +9,15 @@ public class StructurePrebuilder : MonoBehaviour
     [SerializeField] private GameObject _cablePrefab;
 
     private GameObject windmill1;
+    private GameObject windmill2;
     private GameObject _cable;
     private int counter = 0;
     void Start()
     {
         windmill1 = Instantiate(_windmillPrefab,new Vector3(0,PlacementManager.Instance.cellIndicatorPlacementY,0),Quaternion.identity);
-        windmill1.transform.rotation = Quaternion.Euler(-90,180,180);
-        GameObject windmill2 = Instantiate(_windmillPrefab,new Vector3(1,PlacementManager.Instance.cellIndicatorPlacementY,0),Quaternion.identity);
-        windmill2.transform.rotation = Quaternion.Euler(-90,180,180);
+        //windmill1.transform.rotation = Quaternion.Euler(-90,180,180);
+        windmill2 = Instantiate(_windmillPrefab,new Vector3(1,PlacementManager.Instance.cellIndicatorPlacementY,0),Quaternion.identity);
+        //windmill2.transform.rotation = Quaternion.Euler(-90,180,180);
         GridDataManager.SetGridDataAtPos(new Vector3Int(0,0,1),windmill1);
         GridDataManager.SetGridDataAtPos(new Vector3Int(1,0,1),windmill2);
         //_cable = Instantiate(_cablePrefab,Vector3.zero,Quaternion.identity);
@@ -29,6 +30,8 @@ public class StructurePrebuilder : MonoBehaviour
             counter++;
         }
         else if(counter == 10){
+            windmill1.GetComponent<Wandler>().tagTree.SetProductionText(42);
+            windmill2.GetComponent<Wandler>().tagTree.SetProductionText(42);
             ConnectCableMode.Instance.SetStartpoint(windmill1);
             ConnectCableMode.Instance.SetEnpoint(GridDataManager.GetGridDataAtPos(new Vector3Int(2,2,1)));
             _cable =ConnectCableMode.Instance.PlaceCable();

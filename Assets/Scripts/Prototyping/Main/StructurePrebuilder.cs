1b411330f2c62d457325ee1f842128ffa13e7d43
Aloyminium <mwendel888@gmail.com>
2025-01-28 17:35:03 +0100

    fixed: t1 🍔

--- Assets/Scripts/Prototyping/Main/StructurePrebuilder.cs
+++ Assets/Scripts/Prototyping/Main/StructurePrebuilder.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StructurePrebuilder : MonoBehaviour
@@ -14,30 +15,37 @@ public class StructurePrebuilder : MonoBehaviour
     
     void Start()
     {
-        _windmill1 = Instantiate(_windmillPrefab,new Vector3(0,PlacementManager.Instance.cellIndicatorPlacementY,0),Quaternion.Euler(-90,180,180));
+       
+         _windmill1 = Instantiate(_windmillPrefab,new Vector3(0,PlacementManager.Instance.cellIndicatorPlacementY,0),Quaternion.Euler(-90,180,180));
         _windmill2 = Instantiate(_windmillPrefab, new Vector3(1,PlacementManager.Instance.cellIndicatorPlacementY,0),Quaternion.Euler(-90,180,180));
         
         GridDataManager.SetGridDataAtPos(new Vector3Int(0,0,1),_windmill1);
         GridDataManager.SetGridDataAtPos(new Vector3Int(1,0,1),_windmill2);
         GridDataManager.GetGridDataAtPos(new Vector3Int(0, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
-        GridDataManager.GetGridDataAtPos(new Vector3Int(1, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
-        //_cable = Instantiate(_cablePrefab,Vector3.zero,Quaternion.identity);
-
+        
         StartCoroutine(ConnectWindmills());
     }
 
     
     private IEnumerator ConnectWindmills(){
 
-        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();GridDataManager.GetGridDataAtPos(new Vector3Int(1, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
+        //_cable = Instantiate(_cablePrefab,Vector3.zero,Quaternion.identity);
+
         Wandler windmill1Wandler = _windmill1.GetComponent<Wandler>();
         Wandler windmill2Wandler = _windmill2.GetComponent<Wandler>();
+        
         windmill1Wandler.tagTree.SetProductionText(windmill1Wandler.generating);
         windmill2Wandler.tagTree.SetProductionText(windmill2Wandler.generating);
             
         ConnectCableMode.Instance.SetStartpoint(_windmill1);
         ConnectCableMode.Instance.SetEnpoint(GridDataManager.GetGridDataAtPos(new Vector3Int(2,2,1)));
         _cable =ConnectCableMode.Instance.PlaceCable();
+        _cable.GetComponent<Wandler>().addInputWandler(windmill1Wandler);
+        _cable.GetComponent<Wandler>().addOutputWandler(GridDataManager.GetGridDataAtPos(new Vector3Int(2,2,1)).GetComponent<Wandler>());
+        GraphManager.Instance.calculateAll();
         _cable.GetComponent<LineRenderer>().enabled = true;
+
+        
     }
 }

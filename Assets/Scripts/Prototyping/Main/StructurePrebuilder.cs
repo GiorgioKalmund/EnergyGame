20333f99a67fe92087e3d5eb85b246fd8ee09faf
safetymax <maximilian.grummt@gmx.de>
2025-01-29 11:18:26 +0100

    maybe fixed cable in tutorial one during load

--- Assets/Scripts/Prototyping/Main/StructurePrebuilder.cs
+++ Assets/Scripts/Prototyping/Main/StructurePrebuilder.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StructurePrebuilder : MonoBehaviour
 {
@@ -22,14 +24,16 @@ public class StructurePrebuilder : MonoBehaviour
         GridDataManager.SetGridDataAtPos(new Vector3Int(0,0,1),_windmill1);
         GridDataManager.SetGridDataAtPos(new Vector3Int(1,0,1),_windmill2);
         GridDataManager.GetGridDataAtPos(new Vector3Int(0, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
+        GridDataManager.GetGridDataAtPos(new Vector3Int(1, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
         
-        StartCoroutine(ConnectWindmills());
+        StartCoroutine(WaitForSceneLoad());
+        //StartCoroutine(ConnectWindmills());
     }
 
     
     private IEnumerator ConnectWindmills(){
 
-        yield return new WaitForEndOfFrame();GridDataManager.GetGridDataAtPos(new Vector3Int(1, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
+        yield return new WaitForEndOfFrame();
         //_cable = Instantiate(_cablePrefab,Vector3.zero,Quaternion.identity);
 
         Wandler windmill1Wandler = _windmill1.GetComponent<Wandler>();
@@ -47,5 +51,16 @@ public class StructurePrebuilder : MonoBehaviour
         _cable.GetComponent<LineRenderer>().enabled = true;
 
         
+    }
+
+    private IEnumerator WaitForSceneLoad()
+    {
+        // Wait until the scene is fully loaded
+        while (!SceneManager.GetActiveScene().isLoaded)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(ConnectWindmills());
     }
 }

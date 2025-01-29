6f4b8f8666a41339fc959afbd1fcac6ccdbe8706
safetymax <maximilian.grummt@gmx.de>
2025-01-29 12:21:21 +0100

    fix lvl 1 tutorial

--- Assets/Scripts/Prototyping/Main/StructurePrebuilder.cs
+++ Assets/Scripts/Prototyping/Main/StructurePrebuilder.cs
@@ -20,15 +20,26 @@ public class StructurePrebuilder : MonoBehaviour
         _windmill2.GetComponent<Wandler>().tagTree.SetProductionText(19);
         _cable.GetComponent<Wandler>().generating = 16;
         _cable.GetComponent<Wandler>().tagTree.SetProductionText(16);
-        _cable.GetComponent<Wandler>().addOutputWandler(_endpoint.GetComponent<Wandler>());
-        _endpoint.GetComponent<Wandler>().ComputeInput();
 
         GridDataManager.SetGridDataAtPos(new Vector3Int(0,0,1),_windmill1);
         GridDataManager.SetGridDataAtPos(new Vector3Int(1,0,1),_windmill2);
         GridDataManager.GetGridDataAtPos(new Vector3Int(0, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
         GridDataManager.GetGridDataAtPos(new Vector3Int(1, 0, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
+        //GridDataManager.GetGridDataAtPos(new Vector3Int(2,2,1)).GetComponent<Wandler>().generating = 16;
+
+        StartCoroutine(connectEndpoint());
+    }
+
+    private IEnumerator connectEndpoint(){
+        while(!SceneManager.GetActiveScene().isLoaded){
+            yield return null;
+        }
+        yield return new WaitForEndOfFrame();
+
+        _cable.GetComponent<Wandler>().addOutputWandler(_endpoint.GetComponent<Wandler>());
+        _endpoint.GetComponent<Wandler>().ComputeInput();
+
         GridDataManager.SetGridDataAtPos(new Vector3Int(2,2,1),_endpoint);
         GridDataManager.GetGridDataAtPos(new Vector3Int(2, 2, 0)).GetComponent<TileDataWrapper>().tileData.setPlacementType(PlacementType.Blocked);
-        //GridDataManager.GetGridDataAtPos(new Vector3Int(2,2,1)).GetComponent<Wandler>().generating = 16;
     }
 }
